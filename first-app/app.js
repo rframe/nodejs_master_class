f9723557b8933d6499ea04275e5ca1b94b104d1a
russell f <russell.frame@machiningcloud.com>
2018-04-13 11:16:25 -0700

    Node Module System: Event Arguments

--- first-app/app.js
+++ first-app/app.js
@@ -1,12 +1,18 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
+const logger = require('./logger');
 
-const emitter = new EventEmitter();
+const emitter = require('./event');
+// const emitter = new EventEmitter();
 
 const messageLogged = 'messageLogged';
 
 // Register a listener
-emitter.on(messageLogged, () => {
-    console.log('Listener Called');
-});
+// emitter.on(messageLogged, (arg) => { // e, eventArg
+//     console.log(`Listener Called id: ${arg.id} url: ${arg.url}`);
+// });
 
-emitter.emit(messageLogged);
+// emitter.emit(messageLogged, {id: 1, url: 'http://'});
+
+
+// Raise: logging (data: message)
+emitter.emit(logger.logEvent, {message: 'Test Logger'});
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,8 +1,19 @@
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+const emitter = require('./event.js');
+
 var url = 'http://mylogger.io/log';
+
+const logEvent = 'logger.logEvent';
 
 function log(message) {
     // Send an HTTP request
     console.log(message);
 }
 
-module.exports = log;
+emitter.on(logEvent, (arg) => { // e, eventArg
+    console.log(`Log: ${arg.message}`);
+});
+
+module.exports.log = log;
+module.exports.logEvent = logEvent;
