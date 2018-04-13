cfd7602929bb9f893e67c85786dd1581cdc03ae2
russell f <russell.frame@machiningcloud.com>
2018-04-13 10:09:25 -0700

    Node Module System: Creating a Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,13 +1,1 @@
-function sayHello(name) {
-    console.log('Hello ' +name);
-} 
-
-// sayHello('Russell');
-
-var message = '';
-/** Will result in error since with node js, scoped to this file only **/
-// console.log(windows.message);
-
 console.log(module);
-/** Will result in error since with node js, scoped to this file only **/
-// console.log(window.module);
