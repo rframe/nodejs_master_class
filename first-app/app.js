16d5e91d44f26fa556798691533cdba61251c1b2
russell f <russell.frame@machiningcloud.com>
2018-04-13 10:04:08 -0700

    Node Module System: Modules

--- first-app/app.js
+++ first-app/app.js
@@ -4,18 +4,10 @@ function sayHello(name) {
 
 // sayHello('Russell');
 
-// console.log(window);
-
-// setTimeout();
-// clearTimeout();
-
-// setInterval();
-// clearInterval();
-
 var message = '';
 /** Will result in error since with node js, scoped to this file only **/
-console.log(windows.message);
-// global.message;
+// console.log(windows.message);
 
-// window.setTimeout
-// global.setTimeout
+console.log(module);
+/** Will result in error since with node js, scoped to this file only **/
+// console.log(window.module);
