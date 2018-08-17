8d034a27a42fee548a3bb7b477154aa70ef6c3c6
machiningcloud\russell.frame <russell.frame@machiningcloud.com>
2018-08-17 09:54:42 -0700

    Express - Advanced Topics: Creating Custom Middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,15 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const logger = require('./logger');
+const authenticator = require('./authenticator');
+
 
 app.use(express.json());
+
+app.use(logger);
+
+app.use(authenticator);
 
 const courses = [
     {
